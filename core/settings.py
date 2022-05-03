62772830e9cb4c1e9fe994be7f0f011b28a076c1
Patricio <pmoracho@gmail.com>
2022-05-03 00:48:49 -0300

    Movemos todo al puerto 80 y código para detectar IP en un EC2

--- core/settings.py
+++ core/settings.py
@@ -18,8 +18,30 @@ SECRET_KEY = config('SECRET_KEY', default='S#perS3crEt_1122')
 DEBUG = config('DEBUG', default=True, cast=bool)
 
 # load production server from .env
-ALLOWED_HOSTS        = ['localhost', 'localhost:85', '127.0.0.1', config('SERVER', default='127.0.0.1')]
-CSRF_TRUSTED_ORIGINS = ['http://localhost:85', 'http://127.0.0.1', 'https://' + config('SERVER', default='127.0.0.1')]
+ALLOWED_HOSTS        = ['localhost',
+                        'localhost:80',
+                        '127.0.0.1',
+                        config('SERVER', default='127.0.0.1')]
+
+
+
+from urllib.error import HTTPError, URLError
+from urllib.request import urlopen
+
+
+try:
+    with urlopen('http://169.254.169.254/latest/meta-data/public-ipv4', timeout=0.01) as response:
+        EC2_PRIVATE_IP =  response.read().decode("utf-8")
+        ALLOWED_HOSTS.append(EC2_PRIVATE_IP)
+except HTTPError as error:
+    pass
+except URLError as error:
+    pass
+except TimeoutError:
+    pass
+
+
+CSRF_TRUSTED_ORIGINS = ['http://localhost:80', 'http://127.0.0.1', 'https://' + config('SERVER', default='127.0.0.1')]
 
 # Application definition
 
